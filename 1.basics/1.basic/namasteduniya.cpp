fb25fdda04d6a8ba69537b116f6750a7d7f33091
rpm <rishikpuneetm@gmail.com>
2023-02-18 06:48:58 +0530

    Initial commit

---
+++ 1.basics/1.basic/namasteduniya.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a;
+    cout<<"enter  a number:  "<<endl;
+    
+    cin>>a;
+    cout<<"you entered "<<a <<endl;
+    return 0;
+}
