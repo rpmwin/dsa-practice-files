54419bce3871c041e55dd18ab62d9b58acd9df9d
rpm <rishikpuneetm@gmail.com>
2023-02-18 12:38:11 +0530

    linear_searching code done

--- 5.SEARCHING/linear_searching.cpp
+++ 5.SEARCHING/linear_searching.cpp
@@ -7,8 +7,40 @@ int main()
     int size;
     cout<<"enter the size of the array :"<<endl;
     cin>>size;
+    vector<int> arr(size);
+    cout<<"enter the elements of the array :"<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<endl;
+    int findnum;
+    cout<<"enter the number you need to find"<<endl;
+    cin>>findnum;
+
+    //linear search code important
+
+    bool ans=0;
+    for (int i = 0; i < size; i++)
+    {
+        /* code */
+        if (arr[i]==findnum)
+        {
+            cout<<"at "<<i+1<<" target found";
+            break;
+        } 
+        else if (i==size-1)
+        {
+            /* code */
+            ans=1;
+        }
+        
+    }
+    if (ans==1)
+    {
+        /* code */
+        cout<<"invalid number";
+    }
     
- 
- 
     return 0;
 }
